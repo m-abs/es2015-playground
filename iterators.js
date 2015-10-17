b862c29746ca2fbc150f92ef5f56697663716603
Morten Sjøgren <m_abs@mabs.dk>
2015-10-17 13:48:51 +0200

    More iterators

--- iterators.js
+++ iterators.js
@@ -11,6 +11,11 @@ const helper = {
   },
   get set() {
     return new Set(this.array);
+  },
+  get map() {
+    return new Map(this.array.map(function(value) {
+      return [value, value * 1.25];
+    }));
   }
 };
 
@@ -28,15 +33,37 @@ for (const value of generateIterator()) {
   console.log('      ', value);
 }
 
-console.log('  Both arrays and sets are iterable:');
-console.log('    array:');
+console.log('    Arrays, Sets and Maps are iterable:');
+console.log('      array:');
 for (const value of helper.array) {
-  console.log('      ', value);
+  console.log('        ', value);
 }
 
-console.log('    set:');
+console.log('      set:');
 for (const value of helper.set) {
-  console.log('      ', value);
+  console.log('        ', value);
+}
+
+console.log('      map:');
+for (const value of helper.map) {
+  console.log('        ', value[0], '=', value[1]);
+}
+
+console.log('    Making our own iteratable objects:');
+const iteratableObj = {
+  data: helper.array.map(function(v) {
+    return Math.floor(v * 1.323);
+  }),
+  *[Symbol.iterator]() {
+    for (const v of this.data) {
+      yield v;
+    }
+  }
+};
+
+console.log('      for..of iteratableObj:');
+for (const value of iteratableObj) {
+  console.log('        ', value);
 }
 
 console.log('  Without using es2015 features:');
@@ -67,6 +94,44 @@ function generateInteratorOld() {
 const iteratorOld = generateInteratorOld();
 while (true) {
   const data = iteratorOld.next();
+  if (data.done) {
+    break;
+  }
+
+  const value = data.value;
+  console.log('       ', value);
+}
+
+const iteratableObjOld = {
+  data: helper.array.map(function(v) {
+    return Math.floor(v * 1.323);
+  }),
+  iterator: function() {
+    let idx = -1;
+    const self = this;
+    return {
+      next: function() {
+        idx += 1;
+        const output = {
+          done: idx >= self.data.length,
+          value: undefined
+        };
+
+        if (!output.done) {
+          output.value = self.data[idx];
+        } else {
+          idx = self.data.length;
+        }
+
+        return output;
+      }
+    };
+  }
+};
+
+const objIterator = iteratableObjOld.iterator();
+while (true) {
+  const data = objIterator.next();
   if (data.done) {
     break;
   }
